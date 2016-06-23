f4c84836e7d1640d4f05ed454e4b5a9321b60135
Corey Smith <coreyas@gmail.com>
2016-06-23 13:23:19 -0400

    Clean up spacing in IgnitionConstants

--- Ignition.Sc/IgnitionConstants.cs
+++ Ignition.Sc/IgnitionConstants.cs
@@ -1,36 +1,32 @@
 ﻿namespace Ignition.Sc
 {
-    public struct IgnitionConstants
-    {
-        public struct Placeholders
-        {
-            public struct Layout
-            {
-                public const string LayoutContent = "layoutContent";
-                public const string LayoutHead = "layoutHead";
-                public const string LayoutHeader = "layoutHeader";
-                public const string LayoutFrame = "layoutFrame";
-                public const string LayoutFooter = "layoutFooter";
-                public const string LayoutFooterScript = "layoutFooterScript";
-                public const string LayoutPageEditor = "layoutExperienceEditor";
-            }
+	public struct IgnitionConstants
+	{
+		public struct Placeholders
+		{
+			public struct Layout
+			{
+				public const string LayoutContent = "layoutContent";
+				public const string LayoutHead = "layoutHead";
+				public const string LayoutHeader = "layoutHeader";
+				public const string LayoutFrame = "layoutFrame";
+				public const string LayoutFooter = "layoutFooter";
+				public const string LayoutFooterScript = "layoutFooterScript";
+				public const string LayoutPageEditor = "layoutExperienceEditor";
+			}
 
-            public struct Head
-            {
-                public const string HeadMetadata = "headMetadata";
-                public const string HeadSeo = "headSeo";
-                public const string HeadTaxonomy = "headTaxonomy";
-                public const string HeadScript = "headScript";
-                public const string HeadStyle = "headStyle";
-            }
+			public struct Head
+			{
+				public const string HeadMetadata = "headMetadata";
+				public const string HeadSeo = "headSeo";
+				public const string HeadTaxonomy = "headTaxonomy";
+				public const string HeadScript = "headScript";
+				public const string HeadStyle = "headStyle";
+			}
 
-            public struct Footer
-            {
-                
-            }
-            
-
-            
-        }
-    }
+			public struct Footer
+			{
+			}
+		}
+	}
 }
