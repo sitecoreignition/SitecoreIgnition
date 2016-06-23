2244448d84cbe5ad83dea40458fc055fdebe56a4
Jon Upchurch <jon.upchurch@perficient.com>
2016-06-22 20:12:34 -0400

    added missing template and models for existing and added a 2nd backgroundImage template

---
+++ Ignition.Data/Fields/IDateField1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Ignition.Core.Models;
+
+namespace Ignition.Data.Fields
+{
+    [SitecoreType(TemplateId = "{AA715605-C3E3-46EC-BDCB-CBB1365E0D1B}")]
+    public interface IDateField1 : IModelBase
+    {
+        [SitecoreField(FieldId= "{FF8D378C-E9DA-4D74-8913-17D1D729BF2C}")]
+        DateTime DateField1 { get; set; }
+    }
+}
