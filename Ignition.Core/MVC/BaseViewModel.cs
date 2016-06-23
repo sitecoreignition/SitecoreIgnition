ef9362e4a89df1527a9e82a370d78ff2118a324c
Jon Upchurch <jon.upchurch@perficient.com>
2016-06-22 21:11:06 -0400

    removed unneeded fields from viewmodel

--- Ignition.Core/MVC/BaseViewModel.cs
+++ Ignition.Core/MVC/BaseViewModel.cs
@@ -1,14 +1,9 @@
-﻿using System.Collections.Generic;
-using Ignition.Core.Models;
-using Ignition.Core.Models.BaseModels;
-using Ignition.Core.Models.Page;
+﻿using Ignition.Core.Models.Page;
 
 namespace Ignition.Core.Mvc
 {
     public class BaseViewModel
     {
-        private List<string> ErrorMessages { get; } = new List<string>();
-        public string[] Errors => ErrorMessages.ToArray();
         public virtual string ViewPath { get; set; }
         public IPage ContextPage { get; set; }
         public string ParentPlaceholderName { get; set; }
