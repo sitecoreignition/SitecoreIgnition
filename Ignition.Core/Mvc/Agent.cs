5a3c58a13017b4e0d691be0f3a43d7ae1dbbb3b5
Corey Smith <coreyas@gmail.com>
2016-06-26 11:08:00 -0400

    Refactor Agent DataBind method into separate class

--- Ignition.Core/Mvc/Agent.cs
+++ Ignition.Core/Mvc/Agent.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Linq;
-using Ignition.Core.Attributes;
+using System.ComponentModel.Composition;
 using Ignition.Core.Models.BaseModels;
 using Ignition.Core.Models.Page;
 using Ignition.Core.Repositories;
-using Sitecore.Diagnostics;
 
 namespace Ignition.Core.Mvc
 {
 	public abstract class Agent<TViewModel> where TViewModel : BaseViewModel, new()
 	{
+		[Import]
+		protected IViewModelDataBinder ViewModelDataBinder { get; set; }
+
 		public string ViewPath => ViewModel.ViewPath;
 		public TViewModel ViewModel { get; protected set; }
 		public ItemContext Context { get; private set; }
@@ -24,39 +25,17 @@ namespace Ignition.Core.Mvc
 			Context = context;
 			RenderingParameters = Context.RenderingParameters;
 			Datasource = Context.DatasourceItem;
+
 			ViewModel = new TViewModel
 			{
 				ContextPage = Context.ContextPage,
 				ParentPlaceholderName = Context.PlaceholderName
 			};
-			DataBind();
+			ViewModelDataBinder.DataBind(ViewModel, Datasource);
+
 			AgentParameters = Context.AgentParameters;
 		}
 
 	    public abstract void PopulateModel();
-
-		private void DataBind()
-		{
-            if (Datasource.GetType().GetCustomAttributes(typeof(IgnoreAutomapAttribute), true).Any()) return;
-		    try
-		    {
-		        foreach (var prop in ViewModel.GetType().GetProperties()
-		            .Where(
-		                a =>
-		                    typeof (IModelBase).IsAssignableFrom(a.PropertyType) &&
-		                    !(typeof (IPage).IsAssignableFrom(a.PropertyType) ||
-		                      typeof (IParamsBase).IsAssignableFrom(a.PropertyType)
-		                      || a.GetCustomAttributes(typeof(IgnoreAutomapAttribute), true).Any())))
-		            prop.SetValue(ViewModel, Datasource);
-		    }
-		    catch (ArgumentNullException argumentNullException)
-		    {
-		        Log.Error("Databind Error", argumentNullException, this);
-		    }
-		    catch (Exception ex)
-		    {
-                Log.Error("Databind Error", ex, this);
-            }
-		}
 	}
 }
