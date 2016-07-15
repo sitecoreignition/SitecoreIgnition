57c89af58f1489c76d4b300e05c668568f01a4a8
Corey Smith <coreyas@gmail.com>
2016-07-15 18:47:51 -0400

    Reorder properties in AgentContext

--- Ignition.Core/Mvc/AgentContext.cs
+++ Ignition.Core/Mvc/AgentContext.cs
@@ -10,22 +10,22 @@ namespace Ignition.Core.Mvc
     {
         public ISitecoreContext SitecoreContext { get; set; }
 
-        public AgentContext(ControllerContext controllerContext, ISitecoreContext sitecoreContext)
-            : base(controllerContext)
-        {
-            if (sitecoreContext == null) throw new ArgumentNullException(nameof(sitecoreContext));
-            SitecoreContext = sitecoreContext;
-        }
+        public object AgentParameters { get; set; }
+        public IPage ContextPage { get; set; }
+        public IModelBase DatasourceItem { get; set; }
+        public string PlaceholderName { get; set; }
+        public IParamsBase RenderingParameters { get; set; }
 
         private IPage _homeItem;
         public IPage HomeItem => _homeItem ?? (_homeItem = SitecoreContext.GetHomeItem<IPage>(false, true));
 
         public string ModuleWrapperName => Controller?.GetType().Name.Replace("Controller", string.Empty);
 
-        public IPage ContextPage { get; set; }
-        public IModelBase DatasourceItem { get; set; }
-        public IParamsBase RenderingParameters { get; set; }
-        public string PlaceholderName { get; set; }
-        public object AgentParameters { get; set; }
+        public AgentContext(ControllerContext controllerContext, ISitecoreContext sitecoreContext)
+            : base(controllerContext)
+        {
+            if (sitecoreContext == null) throw new ArgumentNullException(nameof(sitecoreContext));
+            SitecoreContext = sitecoreContext;
+        }
     }
 }
