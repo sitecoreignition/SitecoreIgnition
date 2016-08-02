4377e54a817c49f83c0911dc17dbf9912751adb0
Jon Upchurch <jon.upchurch@perficient.com>
2016-08-02 14:44:57 -0400

    removing unnecessary or unused constants.

--- Ignition.Core/CoreConstants.cs
+++ Ignition.Core/CoreConstants.cs
@@ -2,11 +2,6 @@
 {
     public struct CoreConstants
     {
-        public const string IndexFormat = "Coveo_{0}_index";
-        public const string Web = "web";
-        public const string Master = "master";
-        public const string UndefinedComponent = "UndefinedComponent";
-        public const string ErrorViewPath = "~/Views/Shared/Error/ControlError.cshtml";
         public const string SitecoreFallThroughRoute = "scIsFallThrough";
     }
 }
