51d63a1e90ce4d699f701ed548554dfe1e1f6b76
Carlos Rodriguez <carlos.rodriguez@perficient.com>
2016-08-02 13:35:28 -0700

    Adding code which provides About Hero component

---
+++ Ignition.Sc/Components/Hero/AboutHeroAgent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ignition.Core.Mvc;
+
+namespace Ignition.Sc.Components.Hero
+{
+    public class AboutHeroAgent : Agent<AboutHeroViewModel>
+    {
+        public override void PopulateModel()
+        {
+            var ds = Datasource as IAboutHero;
+
+            if (ds == null) return;
+
+            ViewModel.Image = ds;
+            ViewModel.Heading = ds;
+            ViewModel.RichContext1 = ds;
+            ViewModel.LogoImages = ds.LogoImages;
+        }
+    }
+}
---
+++ Ignition.Sc/Components/Hero/HeroController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ignition.Core.Mvc;
+using Ignition.Core.Repositories;
+
+namespace Ignition.Sc.Components.Hero
+{
+    public class HeroController : IgnitionController
+    {
+        public HeroController(ItemContext context) : base(context)
+        {   
+        }
+
+        public ActionResult AboutHero() => View<AboutHeroAgent, AboutHeroViewModel>();
+    }
+}
