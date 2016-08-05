64577e0796ebad25d8d08fa15cdc6b30a3d73ba4
Jon Upchurch <jon.upchurch@perficient.com>
2016-08-05 14:12:06 -0400

    WIP fieldname remapper

---
+++ Ignition.Infrastructure/Pipelines/FieldnameRewriter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sitecore.Diagnostics;
+using Sitecore.Shell.Applications.ContentEditor.Pipelines.RenderContentEditor;
+namespace Ignition.Infrastructure.Pipelines
+{
+    public class FieldnameRewriter 
+    {
+        public void Process(RenderContentEditorArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            var editorFormatter = new EditorFormatter(args);
+            editorFormatter.RenderSections(args.Parent, args.Sections, args.ReadOnly);
+        }
+    }
+}
---
+++ Ignition.Infrastructure/Pipelines/YmlSettingsReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace Ignition.Infrastructure.Pipelines
+{
+    public class YmlSettingsReader
+    {
+	    public YmlSettingsReader()
+	    {
+			var reader = new StringReader(Document);
+			var deserializer = new Deserializer(namingConvention: new CamelCaseNamingConvention());
+			var maps = deserializer.Deserialize<TemplateMap>(reader);
+		}
+
+	    private const string Document = @"---
+            TemplateMap:
+				- TemplateItem: 
+					-TemplateName: CallToAction
+					MapItems:
+						- FieldName: Heading
+						MapTo: Title
+						- FieldName: Subtitle
+						MapTo: Subheading
+			";
+    }
+}
+public class TemplateMap
+{
+	private List<TemplateItem> TemplateItems { get; set; }
+}
+
+public class MapItem
+{
+	public string FieldName { get; set; }
+	public string MapTo { get; set; }
+}
+
+public class TemplateItem
+{
+	public string TemplateName { get; set; }
+	public List<MapItem> MapItems { get; set; }
+}
