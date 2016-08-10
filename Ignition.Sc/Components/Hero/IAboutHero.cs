228166adac46f9831c3547eb3f1a9d8decf77313
Carlos Rodriguez <carlos.rodriguez@perficient.com>
2016-08-10 08:56:24 -0700

    Pushing code changes for this About Page Hero component and Unicorn yml files indication additions of template files and component definition items

---
+++ Ignition.Sc/Components/Hero/IAboutHero.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Ignition.Data.Fields;
+
+namespace Ignition.Sc.Components.Hero
+{
+    [SitecoreType(TemplateId = "{6538AB6D-3016-4708-8800-0735967EA67F}", AutoMap = true)]
+    public interface IAboutHero : IHeading, IImage, IRichContent1
+    {
+        [SitecoreField(FieldId = "{07DF7C11-7EA7-4739-8FE5-9A3DA7067163}", Setting = SitecoreFieldSettings.InferType)]
+        IEnumerable<ILinkedImage> LogoImages { get; set; }
+    }
+}
---
+++ Ignition.Sc/Components/Hero/ILinkedImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Ignition.Data.Fields;
+
+namespace Ignition.Sc.Components.Hero
+{
+    [SitecoreType(TemplateId = "{97F58184-24C4-4296-979E-288AA210BC69}", AutoMap = true)]
+    public interface ILinkedImage : IImage, IPrimaryLink
+    {
+    }
+}
