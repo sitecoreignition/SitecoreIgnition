e8ca425e152ceaf440bec318180ade831973e80c
Jon Upchurch <jon.upchurch@perficient.com>
2016-08-17 15:52:02 -0400

    wip theme

---
+++ Ignition.Sc/Components/Bookends/HeaderViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ignition.Core.Mvc;
+
+namespace Ignition.Sc.Components.Bookends
+{
+	public class HeaderViewModel : BaseViewModel
+	{
+	}
+}
---
+++ Ignition.Sc/Components/Shared/IIgnitionPage.cs
@@ -0,0 +1,10 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using Ignition.Core.Models.Page;
+
+namespace Ignition.Sc.Components.Shared
+{
+	[SitecoreType(TemplateId = "{0ED048AF-E431-40E0-A8A5-3028367FC756}")]
+	public interface IIgnitionPage : IPage
+	{
+	}
+}
