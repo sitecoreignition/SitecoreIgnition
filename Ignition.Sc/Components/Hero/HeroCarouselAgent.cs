80b1336d3bd55d4bda9c86d8ec3534e75d2abb7d
Jon Upchurch <jon.upchurch@perficient.com>
2016-08-18 14:35:49 -0400

    core files

---
+++ Ignition.Sc/Components/Hero/HeroCarouselAgent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ignition.Core.Mvc;
+
+namespace Ignition.Sc.Components.Hero
+{
+	public class HeroCarouselAgent : Agent<HeroCarouselViewModel>
+	{
+		public override void PopulateModel()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Ignition.Sc/Components/Hero/HeroCarouselViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ignition.Core.Mvc;
+
+namespace Ignition.Sc.Components.Hero
+{
+	public class HeroCarouselViewModel : BaseViewModel
+	{
+	}
+}
