413c16481f183df74fdc372476924f2e6cc17e0c
Carlos Rodriguez <carlos.rodriguez@perficient.com>
2016-08-30 09:37:19 -0700

    Updated branch with latest code for content blurb and YML items associated with it.

---
+++ Ignition.Sc/Components/Content/IContentBlurb.cs
@@ -0,0 +1,10 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using Ignition.Data.Fields;
+
+namespace Ignition.Sc.Components.Content
+{
+    [SitecoreType(TemplateId = "{5ED0D95D-4169-4F3F-AC9A-5B30B99E06B1}", AutoMap = true)]
+    public interface IContentBlurb : IHeading, ICopy1
+    {   
+    }
+}
