1583241710f5dda22b87f506528223b2c753c2a6
Jon Upchurch <jon.upchurch@perficient.com>
2016-10-17 14:00:55 -0400

    migrating Metadata

---
+++ Ignition.Feature.Metadata/Controllers/MetadataController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+using Ignition.Foundation.Core.Mvc;
+
+namespace Ignition.Project.IgnitionDemo.Sc.Components.Metadata
+{
+    public class MetadataController : IgnitionController
+    {
+        public ActionResult Head()
+        {
+            return View<BaseViewModel>();
+        }
+        public ActionResult Metadata()
+        {
+            return View<BaseViewModel>();
+        }
+        public ActionResult Script()
+        {
+            return View<BaseViewModel>();
+        }
+        public ActionResult Seo()
+        {
+            return View<BaseViewModel>();
+        }
+        public ActionResult Style()
+        {
+            return View<BaseViewModel>();
+        }
+        public ActionResult Taxonomy()
+        {
+            return View<BaseViewModel>();
+        }
+        public ActionResult Analytics()
+        {
+            return View<BaseViewModel>();
+        }
+
+        public ActionResult PageEdit()
+        {
+            return View<BaseViewModel>();
+        }
+    }
+}
