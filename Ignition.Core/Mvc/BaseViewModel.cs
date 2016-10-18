defe09a9f762d836adcb22a5a0ebee7a83e02ee6
Jon Upchurch <jon.upchurch@perficient.com>
2016-10-18 10:10:58 -0400

    Renamed baseviewmodel glass helpers to avoid common naming conflicts

--- Ignition.Core/Mvc/BaseViewModel.cs
+++ Ignition.Core/Mvc/BaseViewModel.cs
@@ -9,8 +9,8 @@ namespace Ignition.Foundation.Core.Mvc
         public IPage ContextPage { get; set; }
         public string ParentPlaceholderName { get; set; }
         public bool UseWrapper { get; set; }
-		public object CssClass(string cssClass) => new { @class = cssClass };
-	    public object Image(string cssClass, int height, int width) => new { @class = cssClass, height = height.ToString(), width = width.ToString() };
-	    public NameValueCollection Attribute(string attributeName, string attributeValue) => new NameValueCollection { { attributeName, attributeValue } };
+		public object GlassCssClassParameters(string cssClass) => new { @class = cssClass };
+	    public object GlassImageParameters(string cssClass, int height, int width) => new { @class = cssClass, height = height.ToString(), width = width.ToString() };
+	    public NameValueCollection GlassAttributeParameters(string attributeName, string attributeValue) => new NameValueCollection { { attributeName, attributeValue } };
     }
 }
