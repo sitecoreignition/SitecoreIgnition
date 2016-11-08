f24d4e1a1a99f5a7f5aa3e380f0f5b6523a1621a
Jon Upchurch <jon.upchurch@perficient.com>
2016-11-08 15:38:13 -0500

    More view vixes

---
+++ Ignition.Sc/Controllers/MetadataController.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+using Ignition.Foundation.Core.Mvc;
+using IgnitionController = Ignition.Foundation.Core.Mvc.IgnitionController;
+
+namespace Ignition.Project.IgnitionDemo.Sc.Controllers
+{
+    public class MetadataController : IgnitionController
+    {
+        public ActionResult Head() => View<IgnitionViewModel>();
+
+	    public ActionResult Metadata() => View<IgnitionViewModel>();
+
+	    public ActionResult Script()
+        {
+            return View<IgnitionViewModel>();
+        }
+        public ActionResult Seo()
+        {
+            return View<IgnitionViewModel>();
+        }
+        public ActionResult Style()
+        {
+            return View<IgnitionViewModel>();
+        }
+        public ActionResult Taxonomy()
+        {
+            return View<IgnitionViewModel>();
+        }
+        public ActionResult Analytics()
+        {
+            return View<IgnitionViewModel>();
+        }
+
+        public ActionResult PageEdit()
+        {
+            return View<IgnitionViewModel>();
+        }
+    }
+}
